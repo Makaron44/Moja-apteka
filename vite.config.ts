d33694e2e36eccbd1501f11cc1ecb388331444f1
Makaron44 <99068890+Makaron44@users.noreply.github.com>
2026-02-08 19:00:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 'base' musi odpowiadać nazwie Twojego repozytorium na GitHubie
+  base: '/Moja-apteka/',
   define: {
     'process.env': {}
   }
